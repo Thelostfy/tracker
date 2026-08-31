eb22f56e398543d51d775dd3fea5dc12219f8841
TheLostFy <1arian.salehi1389@gmail.com>
2026-08-31 15:56:24 +0330

    please fix my problem

--- app.js
+++ app.js
@@ -5,75 +5,75 @@ const tablebody = document.getElementById("table-body");
 
 function GetData() {
     fetch("https://mooni-expense.azurewebsites.net/api/v1/Transactions")
-    .then((response) => response.json())
-    .then((data) => {
-        let SumDarAmadHa = 0;
-        let SumHazineHa = 0;
-        let sum = 0;
-        data.forEach(element => {
+        .then((response) => response.json())
+        .then((data) => {
+            let SumDarAmadHa = 0;
+            let SumHazineHa = 0;
+            let sum = 0;
+            data.forEach(element => {
 
-            // ----------------------Calculate Sum
-            if (element.type === "Expense") {
-                sum -= element.amount;
-                SumHazineHa += element.amount;
-            }
-            else {
-                sum += element.amount;
-                SumDarAmadHa += element.amount;
-            }
+                // ----------------------Calculate Sum
+                if (element.type === "Expense") {
+                    sum -= element.amount;
+                    SumHazineHa += element.amount;
+                }
+                else {
+                    sum += element.amount;
+                    SumDarAmadHa += element.amount;
+                }
 
-            // Define row for data
-            const card = document.createElement("tr");
-            card.innerHTML = `
+                // Define row for data
+                const card = document.createElement("tr");
+                card.innerHTML = `
                 <td> ${element.type}</td>
                 <td>${element.name}</td>                
                 <td> ${element.category || element.source}</td>
                 <td>${element.amount}</td>
                 `;
 
-            tablebody.appendChild(card);
-        });
+                tablebody.appendChild(card);
+            });
 
-        // define span for exp and income and balance
-        const spanB = document.createElement("span");
-        const spanE = document.createElement("span");
-        const spanI = document.createElement("span");
+            // define span for exp and income and balance
+            const spanB = document.createElement("span");
+            const spanE = document.createElement("span");
+            const spanI = document.createElement("span");
 
-        // ---------------------------Balance
-        spanB.innerHTML = `
+            // ---------------------------Balance
+            spanB.innerHTML = `
             Balance: <strong> ${sum}</strong>`;
 
-        // ---------------------------Income
-        spanI.innerHTML = `
+            // ---------------------------Income
+            spanI.innerHTML = `
             Income: <strong> ${SumDarAmadHa}</strong>`;
 
-        // ---------------------------expencive
-        spanE.innerHTML = `
+            // ---------------------------expencive
+            spanE.innerHTML = `
             total expencive: <strong> ${SumHazineHa}</strong>`;
 
 
-        // add to html
-        balance.appendChild(spanB);
-        totalIncome.appendChild(spanI);
-        totalExpencive.appendChild(spanE);
+            // add to html
+            balance.appendChild(spanB);
+            totalIncome.appendChild(spanI);
+            totalExpencive.appendChild(spanE);
 
 
 
 
 
-    });
+        });
 }
-// GetData();
+GetData();
 const expenciveForm = document.getElementById("expenciveForm");
+
 expenciveForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
+    const expenseModal = new bootstrap.Modal('#addExpense');
+    expenseModal.show();
 
     const data = new FormData(event.target);
     const submitedExpForm = Object.fromEntries(data.entries());
-
-    debugger
-
     fetch("https://mooni-expense.azurewebsites.net/api/v1/Transactions/expense", {
         method: "POST",
         headers: {
@@ -84,39 +84,39 @@ expenciveForm.addEventListener("submit", function (event) {
         .then((response) => response.json())
         .then((data) => {
             console.log("Expencive Added : " + data);
+            expenseModal.hide();
+            GetData();
+
         }
         )
-
-        GetData();
-
 })
 
 
 //income post
 
-const incomeform = document.getElementById("incomeform");
-expenciveForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+// const incomeform = document.getElementById("incomeform");
+// incomeform.addEventListener("submit", function (event) {
+//     event.preventDefault();
 
 
-    const data = new FormData(event.target);
-    const submitedExpForm = Object.fromEntries(data.entries());
+//     const data = new FormData(event.target);
+//     const submitedExpForm = Object.fromEntries(data.entries());
 
-    debugger
+//     debugger;
 
-    fetch("https://mooni-expense.azurewebsites.net/api/v1/Transactions/expense", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(submitedExpForm)
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("Income Added : " + data);
-        }
-        )
+//     fetch("https://mooni-expense.azurewebsites.net/api/v1/Transactions/income", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(submitedExpForm)
+//     })
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log("Income Added : " + data);
+//         }
+//         )
 
-        GetData();
+//         GetData();
 
-})
+// })
