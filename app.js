c21445a24bda7ce78a6d951406b94f3d00e315a2
TheLostFy <1arian.salehi1389@gmail.com>
2026-09-11 20:38:26 +0330

    Fixing the edit expense modal and running into a problem with the edit income modal.

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const totalIncome = document.querySelector("#totalIncome");
 const totalExpencive = document.querySelector("#totalExpencive");
 const balance = document.querySelector("#Balance");
 const tablebody = document.querySelector("#table-body");
+
 const expenseModal = new bootstrap.Modal('#addExpense');
-const expenseForm = document.querySelector("#expenciveForm");
 const incomeModal = new bootstrap.Modal('#addIncome');
+const editExpenseModal = new bootstrap.Modal('#editExpenseModal');
+const editIncomeModal = new bootstrap.Modal('#editIncomeModal');
+
+
+const expenseForm = document.querySelector("#expenciveForm");
 const incomeform = document.querySelector("#incomeform");
+
 const search = document.querySelector("#search");
+
 const editIncomeAmount = document.querySelector("#editIncomeAmount");
+const editIncomeSource = document.querySelector("#editIncomeCategory");
+
 const editExpenseAmount = document.querySelector("#editExpenseAmount");
 const editExpenseName = document.querySelector("#editExpenseName");
+const editExpenseCategory = document.querySelector("#editExpenseCategory");
 
 const AllTransactions = "https://mooni-expense.azurewebsites.net/api/v1/Transactions/";
 const ExpensePost = "https://mooni-expense.azurewebsites.net/api/v1/Transactions/expense/";
@@ -51,7 +61,7 @@ const deleteDataById = async function (ID) {
 
 
 // Get Data From API Function
-function render(inputArray) {
+async function render(inputArray) {
 
     //clear table content
 
@@ -85,9 +95,7 @@ function render(inputArray) {
             console.error("خطا در محاسبه مقادیر:", error);
         }
         let IsExpence = (row.type == "Expense") ? "editExpenseModal" : "editIncomeModal";
-        editIncomeAmount.value = row.amount;
-        editExpenseAmount.value = row.amount;
-        editExpenseName.value = row.name;
+
 
         // Define row for data
         const card = document.createElement("tr");
@@ -98,7 +106,7 @@ function render(inputArray) {
                 <td>${row.amount}</td>
                 <td>
                 <button class="delete-btn btn btn-danger" data-id="${row.id}"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M13.39 17.36L10.64 14.61" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/><path d="M13.36 14.64L10.61 17.39" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/><path d="M8.81 2L5.19 5.63" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/><path d="M15.19 2L18.81 5.63" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/><path d="M2 7.84998C2 5.99998 2.99 5.84998 4.22 5.84998H19.78C21.01 5.84998 22 5.99998 22 7.84998C22 9.99998 21.01 9.84998 19.78 9.84998H4.22C2.99 9.84998 2 9.99998 2 7.84998Z" stroke="currentColor" stroke-width="1.5"/><path d="M3.5 10L4.91 18.64C5.23 20.58 6 22 8.86 22H14.89C18 22 18.46 20.64 18.82 18.76L20.5 10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/></svg></button>
-                    <button class="edit-btn btn btn-danger" data-type="${row.type}" data-bs-toggle="modal" data-bs-target="#${IsExpence}">EDIT</button>
+                    <button class="edit-btn btn btn-danger" data-type="${row.type}" data-name="${row.name}" data-category="${row.category}" data-source="${row.source}" data-amount="${row.amount}">EDIT</button>
                 </td>`;
 
         tablebody.appendChild(card);
@@ -127,15 +135,13 @@ function render(inputArray) {
     totalIncome.appendChild(spanI);
     totalExpencive.appendChild(spanE);
 
-    defineDeleteBtn();
-    defineEditBtn();
+    await defineDeleteBtn();
+    await defineEditBtn();
 
 };
 
-
-
 // Call Data from API
-let a = window.addEventListener("load",
+window.addEventListener("load",
     async function () {
         await fetchdata();
         await render(Transactions)
@@ -143,8 +149,6 @@ let a = window.addEventListener("load",
 
     }
 )
-a
-
 
 // //expence post
 expenseForm.addEventListener("submit", function (event) {
@@ -230,56 +234,43 @@ const defineDeleteBtn = function () {
     }
 
 }
+
+
 const defineEditBtn = function () {
     const EditBtn = document.getElementsByClassName("edit-btn");
     for (const btn of EditBtn) {
         btn.addEventListener("click", async function (event) {
             event.preventDefault();
 
-
-            const editData = new FormData(event.target);
-            const submitedEditForm = Object.fromEntries(editData.entries());
-
-
-            const id = event.target.getAttribute("data-id");
             const type = event.target.getAttribute("data-type");
+            const id = event.target.getAttribute("data-id");
+            const category = event.target.getAttribute("data-category");
+            const amount = event.target.getAttribute("data-amount");
+            const source = event.target.getAttribute("data-source");
+        
 
             if (type == "Expense") {
-                await fetch(ExpensePost + `${id}`, {
-                    method: "PUT",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(submitedEditForm)
-                })
-                    .then((response) => response.json())
-                    .then(async (data) => {
-                        expenseForm.reset();
-                        expenseModal.hide();
-                        await fetchdata();
-                        render(Transactions);
-                    })
+
+                const currentName = event.target.getAttribute("data-name");
+
+                editExpenseName.value = currentName;
+                editExpenseAmount.value = amount;
+                editExpenseCategory.value = category;
+
+                await editExpenseModal.show();
+
             }
-            // else {
-            //     await fetch(IncomePost + `${id}`, {
-            //         method: "PUT",
-            //         headers: {
-            //             "Content-Type": "application/json"
-            //         },
-            //         body: JSON.stringify(submitedEditForm)
-            //     })
-            //         .then((response) => response.json())
-            //         .then(async (data) => {
-            //             expenseForm.reset();
-            //             expenseModal.hide();
-            //             await fetchdata();
-            //             render(Transactions);
-            //         })
-            // }
+            else {
+                editIncomeAmount.value = amount;
+                editIncomeSource.value = category;
+
+                await editIncomeModal.show();
 
 
+            }
         }
         );
+
     }
 
 }
